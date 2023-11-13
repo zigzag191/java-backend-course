9427c9258fba8a49b0e8afa5599d74a4492a5d53
zigzag <wwe.15@list.ru>
2023-11-13 10:18:24 +0300

    add hw6

---
+++ src/main/java/edu/hw6/Task1/DiskMap.java
@@ -0,0 +1,75 @@
+package edu.hw6.Task1;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class DiskMap implements Map<String, String> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public String get(Object key) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String put(String key, String value) {
+        return null;
+    }
+
+    @Override
+    public String remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends String, ? extends String> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @NotNull
+    @Override
+    public Set<String> keySet() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Collection<String> values() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return null;
+    }
+
+}
