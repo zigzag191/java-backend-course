0e5bc417c9a8def8490b1ac3b4885d50604ff0bf
zigzag <wwe.15@list.ru>
2023-11-16 23:18:36 +0300

    printf debugging GitHub actions

--- src/test/java/edu/hw6/Task2Test.java
+++ src/test/java/edu/hw6/Task2Test.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -17,27 +19,38 @@ public class Task2Test extends FileIOTestBase {
         }
     }
 
+    static List<String> getDirectoryContents(Path directory) throws IOException {
+        try (var stream = Files.newDirectoryStream(directory)) {
+            var result = new ArrayList<String>();
+            stream.iterator().forEachRemaining(p -> result.add(p.getFileName().toString()));
+            return result;
+        }
+    }
+
     @Test
-    void copiesShouldBeCreatedWithCorrectName() throws IOException {
+    void copiesShouldBeCreatedWithCorrectNames() throws IOException {
         var testFile = TEMP_DIRECTORY.resolve("test");
         createFile(testFile);
 
         Task2.clonePath(testFile);
-        var copy = TEMP_DIRECTORY.resolve("test - копия");
-        assertThat(Files.exists(copy)).isTrue();
-        assertThat(Files.mismatch(testFile, copy)).isEqualTo(-1);
-
-        Task2.clonePath(copy);
-        var copyCopy = TEMP_DIRECTORY.resolve("test - копия - копия");
-        assertThat(Files.exists(copyCopy)).isTrue();
-        assertThat(Files.mismatch(testFile, copyCopy)).isEqualTo(-1);
-
-        for (int i = 0; i < 10; ++i) {
-            Task2.clonePath(testFile);
-            copy = TEMP_DIRECTORY.resolve("test - копия " + "(" + (i + 2) + ")");
-            assertThat(Files.exists(copy)).isTrue();
-            assertThat(Files.mismatch(testFile, copy)).isEqualTo(-1);
-        }
+        Task2.clonePath(testFile);
+        var contents = getDirectoryContents(TEMP_DIRECTORY);
+        assertThat(contents).containsExactlyInAnyOrder("test - копия", "test - копия (2)", "test");
+//        var copy = TEMP_DIRECTORY.resolve("test - копия");
+//        assertThat(Files.exists(copy)).isTrue();
+//        assertThat(Files.mismatch(testFile, copy)).isEqualTo(-1);
+//
+//        Task2.clonePath(copy);
+//        var copyCopy = TEMP_DIRECTORY.resolve("test - копия - копия");
+//        assertThat(Files.exists(copyCopy)).isTrue();
+//        assertThat(Files.mismatch(testFile, copyCopy)).isEqualTo(-1);
+//
+//        for (int i = 0; i < 10; ++i) {
+//            Task2.clonePath(testFile);
+//            copy = TEMP_DIRECTORY.resolve("test - копия " + "(" + (i + 2) + ")");
+//            assertThat(Files.exists(copy)).isTrue();
+//            assertThat(Files.mismatch(testFile, copy)).isEqualTo(-1);
+//        }
     }
 
     @Test
