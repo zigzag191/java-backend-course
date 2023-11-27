b4835891b3f256e28237b444f7187c0024fa2fe5
zigzag <wwe.15@list.ru>
2023-11-27 23:25:07 +0300

    добавил try-finally блок для ReadWriteLock в PersonDatabase

--- src/main/java/edu/hw7/Task3/RWLockedPersonDatabase.java
+++ src/main/java/edu/hw7/Task3/RWLockedPersonDatabase.java
@@ -11,38 +11,56 @@ public class RWLockedPersonDatabase extends PersonDatabaseImpl {
     @Override
     public void add(Person person) {
         rwl.writeLock().lock();
-        super.add(person);
-        rwl.writeLock().unlock();
+        try {
+            super.add(person);
+        } finally {
+            rwl.writeLock().unlock();
+        }
     }
 
     @Override
     public void delete(int id) {
         rwl.writeLock().lock();
-        super.delete(id);
-        rwl.writeLock().unlock();
+        try {
+            super.delete(id);
+        } finally {
+            rwl.writeLock().unlock();
+        }
     }
 
     @Override
     public List<Person> findByName(String name) {
         rwl.readLock().lock();
-        var result = super.findByName(name);
-        rwl.readLock().unlock();
+        List<Person> result;
+        try {
+            result = super.findByName(name);
+        } finally {
+            rwl.readLock().unlock();
+        }
         return result;
     }
 
     @Override
     public List<Person> findByAddress(String address) {
         rwl.readLock().lock();
-        var result = super.findByAddress(address);
-        rwl.readLock().unlock();
+        List<Person> result;
+        try {
+            result = super.findByAddress(address);
+        } finally {
+            rwl.readLock().unlock();
+        }
         return result;
     }
 
     @Override
     public List<Person> findByPhone(String phone) {
         rwl.readLock().lock();
-        var result = super.findByPhone(phone);
-        rwl.readLock().unlock();
+        List<Person> result;
+        try {
+            result = super.findByPhone(phone);
+        } finally {
+            rwl.readLock().unlock();
+        }
         return result;
     }
 
